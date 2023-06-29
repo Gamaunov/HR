1e738bdf6236292de11de18413a5161fb7db53c2
denis <gamaunov1911@gmail.com>
2023-06-29 15:54:52 +0300

    created Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './components/pages/MainPage/MainPage.async'
-import './index.scss'
-import { Suspense } from 'react'
+import React, { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
+import { MainPageAsync } from './components/pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async'
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
--- src/components/pages/AboutPage/AboutPage.tsx
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -1,4 +1,3 @@
-
 const AboutPage = () => {
   return <div>ABOUT PAGE</div>
 }
