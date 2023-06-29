ec8d00e9e3169cf9e3a439cdaeaabe49a8d19c21
denis <gamaunov1911@gmail.com>
2023-06-29 15:14:02 +0300

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import Counter from './components/Counter'
+import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './components/pages/MainPage/MainPage.async'
 import './index.scss'
+import { Suspense } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="app">
-      hey
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
---
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,6 @@
+
+const AboutPage = () => {
+  return <div>ABOUT PAGE</div>
+}
+
+export default AboutPage
