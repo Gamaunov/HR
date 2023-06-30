87e3c54d92c7329bb7ac1b7158bfab4657612f6d
denis <gamaunov1911@gmail.com>
2023-06-30 12:29:47 +0300

    add svgLoader, fileLoader

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,7 +1,15 @@
-declare module '*.scss' {
+declare module "*.scss" {
   interface IClassNames {
-    [className: string]: string
+    [className: string]: string;
   }
-  const classnames: IClassNames
-  export = classnames
+
+  const classnames: IClassNames;
+  export = classnames;
+}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
 }
