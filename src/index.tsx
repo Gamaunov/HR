01897b50b4a3d2fa2822165cc2416f9b4c193141
Gamayunov <gamaunov1911@gmail.com>
2023-07-06 20:39:29 +0300

    add Modal, Portal, ButtonTheme

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './shared/config/i18n/i18n';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'app/styles/index.scss';
+
 render(
 	<BrowserRouter>
 		<ThemeProvider>
