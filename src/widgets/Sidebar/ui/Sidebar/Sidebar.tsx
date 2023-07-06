ad2905eda61234481e609dd3e6257e06d6d99336
Gamayunov <gamaunov1911@gmail.com>
2023-07-06 11:52:09 +0300

    fix ci

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ui/ThemeSwitcher';
+import { LangSwitcher } from 'shared/ui/langSwitcher/LangSwitcher';
 
 import cls from './Sidebar.module.scss';
-import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
 	className?: string;
