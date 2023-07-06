da812cf4081b3f9664d2031faf97ad5c1fc8b36f
Gamayunov <gamaunov1911@gmail.com>
2023-07-06 13:24:38 +0300

    upgrade Sidebar, Button

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,13 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+	paths,
+	isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
 	const plugins = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
@@ -21,9 +24,11 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
 	if (isDev) {
 		plugins.push(new webpack.HotModuleReplacementPlugin());
-		plugins.push(new BundleAnalyzerPlugin({
-			openAnalyzer: false,
-		}));
+		plugins.push(
+			new BundleAnalyzerPlugin({
+				openAnalyzer: false,
+			})
+		);
 	}
 
 	return plugins;
--- src/shared/ui/langSwitcher/LangSwitcher.tsx
+++ src/shared/ui/langSwitcher/LangSwitcher.tsx
@@ -1,26 +1,28 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
-
-import cls from './LangSwitcher.module.scss';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps {
 	className?: string;
+	short?: boolean;
 }
 
-export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
 	const { t, i18n } = useTranslation();
-	const toggle = async (): Promise<void> => {
+
+	const toggle = async () => {
 		i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
 	};
+
 	return (
 		<Button
-			className={classNames(cls.LangSwitcher, {}, [className])}
-			theme={ThemeButton.CLEAR}
+			className={classNames('', {}, [className])}
+			theme={ButtonTheme.CLEAR}
 			onClick={toggle}
 		>
-			{t('Язык')}
+			{t(short ? 'Короткий язык' : 'Язык')}
 		</Button>
 	);
 };
